80b7f72d481b3381039d95469696d26cfb4184ad
Leonardo Sete <leonardo_o_sete_lab2dev@whirlpool.com>
2025-03-20 16:06:59 -0300

    ci: testando a criação do lambda do garantia digital - ajuste 7

--- infra/main.tf
+++ infra/main.tf
@@ -5,36 +5,22 @@ terraform {
       version = "~> 5.0"
     }
   }
-  # Caso use o backend remoto (S3) num arquivo "backend.tf", não repita aqui
 }
 
 provider "aws" {
   region = "us-east-1"
 }
 
-variable "lambda_version" {
-  type        = string
-  description = "Versão para o nome do Lambda"
-}
-
-variable "email_smtp" {
-  type      = string
-  sensitive = true
-}
-
-variable "pass_smtp" {
-  type      = string
-  sensitive = true
-}
-
 resource "aws_lambda_function" "garantia_digital" {
+  # Usa a var.lambda_version, mas NÃO a declara aqui
   function_name = "garantia-digital-${replace(var.lambda_version, ".", "-")}"
-  # Use a role ARN criada no script bash
-  role          = "arn:aws:iam::114284751948:role/LambdaGarantiaDigitalRole"
-  
-  runtime       = "python3.9"
-  handler       = "create-garantia.lambda_handler"
-  filename      = "../lambda_function_payload.zip"
+
+  # Substitua esse ARN pela role do Lambda já existente
+  role = "arn:aws:iam::114284751948:role/LambdaGarantiaDigitalRole"
+
+  runtime  = "python3.9"
+  handler  = "create-garantia.lambda_handler"
+  filename = "../lambda_function_payload.zip"
 
   source_code_hash = filebase64sha256("../lambda_function_payload.zip")
 
